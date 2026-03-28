bd5a72b12e761d4b42109c7e11735772fa692dc0
Ananya419 <shahiananya381@gmail.com>
2026-03-29 01:08:08 +0530

    Deploy to gh-pages with .nojekyll

---
+++ _next/static/Fkx2dQdW29BFVfBmv8WJ2/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/itzfizz-hero//_next/:path+",
+        "destination": "/itzfizz-hero/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
